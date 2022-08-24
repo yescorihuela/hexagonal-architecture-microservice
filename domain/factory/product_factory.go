e5eef1951c9ff45a0738f41e0dbc6e4bbc999dd6
Yrvin Escorihuela <yrvin.escorihuela@gmail.com>
2022-08-24 08:52:37 -0400

    First commit

---
+++ domain/factory/product_factory.go
@@ -0,0 +1,7 @@
+package factory
+
+import "github.com/yescorihuela/agrak/domain/entity"
+
+func NewProduct() *entity.Product {
+	return nil
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/yescorihuela/agrak
+
+go 1.17
