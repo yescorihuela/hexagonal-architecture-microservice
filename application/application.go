09e36effd0c5c56f1acfe2c1a93dd61218efa09b
Yrvin Escorihuela <yrvin.escorihuela@gmail.com>
2022-08-25 21:36:59 -0400

    Add some comments

--- application/application.go
+++ application/application.go
@@ -3,8 +3,8 @@ package application
 import "strconv"
 
 func Run() error {
-	serverHost := "localhost"
-	serverPort, _ := strconv.ParseUint("8000", 10, 0)
+	serverHost := "localhost"                         // This value would be defined by envvar
+	serverPort, _ := strconv.ParseUint("8000", 10, 0) // This value would be defined by envvar
 	server := NewServer(serverHost, uint(serverPort))
 	return server.Run()
 }
